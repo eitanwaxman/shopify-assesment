9f1582aae2b67db06fe2c66d60a6376e2ee626a9
langroops <langroops@gmail.com>
2022-06-06 08:08:01 -0400

    title and port env added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 const axios = require("axios").default;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
